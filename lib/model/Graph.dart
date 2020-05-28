b1f13c376cea3fdd3b89d3889f98559e4c155334
josephrojas <36609457+josephrojas@users.noreply.github.com>
2020-05-27 23:15:26 -0500

    prros

--- lib/model/Graph.dart
+++ lib/model/Graph.dart
@@ -1,15 +1,53 @@
 class Graph {
-  var maxVertices;
-  List<List<int>> matrix;
+  int maxVertices = 8;
+  List<List<int>> adjacentMatrix;
+  List<List<int>> weightMatrix;
+  List<List<int>> cities;
   Graph(){
+
+
+  }
+  //Lista de convenciones
+  // A = Oviedo
+  // B = Bilbao
+  // C = Madrid
+  // D = Sevilla
+  // E = Alicante
+  // F = Barcelona
+  // G = Malaga
+  // H = Melilla
+
+  //Matriz de adyacencia
+  //  A  B  C  D  E  F  G  H
+  //A[0][0][1][0][0][0][0][0]
+  //B[0][0][1][0][0][1][0][0]
+  //C[1][1][0][1][1][1][1][0]
+  //D[0][0][1][0][0][0][1][0]
+  //E[0][0][1][0][0][1][0][0]
+  //F[0][1][1][0][1][0][1][0]
+  //G[0][0][1][1][0][1][0][1]
+  //H[0][0][0][0][0][0][1][0]
+
+  //Matriz de pesos
+  //  A     B   C   D    E    F    G   H
+  //A[0][0][445][0][0][0][0][0]
+  //B[0][0][395][0][0][606][0][0]
+  //C[445][395][0][531][437][622][534][0]
+  //D[0][0][531][0][0][0][207][0]
+  //E[0][0][437][0][0][538][0][0]
+  //F[0][606][622][0][538][0][1006][0]
+  //G[0][0][534][207][0][1006][0][221]
+  //H[0][0][0][0][0][0][221][0]
+
+  void fillMatrix () {
 
 
   }
 
   dijkistra(int start) {
-    List<int> distance;
-    List<int> padre;
-    List<bool> visto;
+    List<int> distance = new List (maxVertices);
+    List<int> padre = new List (maxVertices);
+    List<bool> visto ;
     for (int i = 0; i < maxVertices; i++) {
       distance[i] = 1200000000;
       padre[i] = -1;
